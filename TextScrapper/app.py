cb29bb6357455e2db20b2fe94d1334d309f188c2
Shivam Rukhaiyar <shivamkk32@gmail.com>
2021-01-11 01:10:48 +0530

    Text Scrapper

---
+++ TextScrapper/app.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template,request
+from urllib.request import   urlopen as uReq
+from bs4 import BeautifulSoup as bs
+
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result',methods=['POST'])
+def route():
+    productName=request.form['content'].replace(' ','_')
+    try:
+        reviews=[]
+        wikiURL="https://en.wikipedia.org/wiki/" +productName
+        uClient=uReq(wikiURL)  #Open the URL
+        fillPage=uClient.read() #Get complete webpage
+        uClient.close()
+        wikiHtml=bs(fillPage,'html.parser')
+        description = wikiHtml.find_all('div', {'id': "mw-content-text"})
+        for i in description:
+
+             try:
+                definition=i.select_one("p:nth-of-type(3)").text
+             except:
+                 definition='No information on this topic'
+             mydict = {"Product": definition, "Name": "name", "Rating": "rating", "CommentHead": "commentHead",
+                  "Comment": "custComment"}  # saving that detail to a dictionary
+        #x = table.insert_one(mydict)  # insertig the dictionary containing the rview comments to the collection
+        reviews.append(mydict)
+        return render_template('results.html', reviews=reviews)
+    except:
+        return 'something is wrong'
+
+
+if __name__== '__main__':
+    app.run()
